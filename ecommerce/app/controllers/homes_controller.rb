17ccf5806877ee311a7ecaac11fb0c8fe08a683d
aruprakshit <aruprakshit@rocketmail.com>
2025-09-20 23:40:25 +0530

    Change the logic of showing featured items.

--- ecommerce/app/controllers/homes_controller.rb
+++ ecommerce/app/controllers/homes_controller.rb
@@ -1,6 +1,9 @@
 class HomesController < ApplicationController
   def index
     @categories = Category.all
-    @featured_items = Item.limit(4)
+    @featured_items = Item.joins(:promotions)
+                          .where(promotions: Promotion.active)
+                          .distinct
+                          .limit(4)
   end
 end
