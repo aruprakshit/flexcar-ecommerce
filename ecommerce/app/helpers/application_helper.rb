137b95190f15428d2adc65e7bc3bcda38663c37f
aruprakshit <aruprakshit@rocketmail.com>
2025-09-20 16:04:50 +0530

    Fix cart count Use currency helper method to display current in the INR format.

--- ecommerce/app/helpers/application_helper.rb
+++ ecommerce/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Format INR amount with proper delimiters
+  def format_currency_inr(inr_amount)
+    "₹#{number_with_delimiter(inr_amount.round(2))}"
+  end
+  
+  # Format cart count as whole number
+  def format_cart_count(count)
+    count.to_i
+  end
 end
