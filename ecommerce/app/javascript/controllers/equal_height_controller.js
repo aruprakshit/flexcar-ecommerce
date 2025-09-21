0787331465b30d7bb307799020280ffa4f3049ea
aruprakshit <aruprakshit@rocketmail.com>
2025-09-21 16:38:24 +0530

    Use Stimulus JS instead of vaniall JS.

---
+++ ecommerce/app/javascript/controllers/equal_height_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["card"]
+
+  connect() {
+    console.log('Equal height controller connected')
+    this.equalizeHeights()
+    this.setupResizeListener()
+  }
+
+  disconnect() {
+    if (this.resizeTimeout) {
+      clearTimeout(this.resizeTimeout)
+    }
+  }
+
+  equalizeHeights() {
+    const cards = this.cardTargets.flatMap(target => 
+      Array.from(target.querySelectorAll('.card'))
+    )
+    
+    console.log('Found cards:', cards.length)
+    if (cards.length === 0) return
+    
+    cards.forEach(card => {
+      card.style.height = 'auto'
+    })
+    
+    for (let i = 0; i < cards.length; i += 4) {
+      const row = cards.slice(i, i + 4)
+      let maxHeight = 0
+      
+      row.forEach(card => {
+        const cardHeight = card.offsetHeight
+        if (cardHeight > maxHeight) {
+          maxHeight = cardHeight
+        }
+      })
+      
+      row.forEach(card => {
+        card.style.height = maxHeight + 'px'
+      })
+    }
+  }
+
+  setupResizeListener() {
+    this.handleResize = () => {
+      clearTimeout(this.resizeTimeout)
+      this.resizeTimeout = setTimeout(() => {
+        this.equalizeHeights()
+      }, 100)
+    }
+    
+    window.addEventListener('resize', this.handleResize)
+  }
+}
